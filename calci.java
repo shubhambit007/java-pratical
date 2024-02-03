0c727b0444abe846f3bb20388b3fb6b22b9045cc
shubham zirpe <155884711+shubhambit007@users.noreply.github.com>
2024-02-03 15:07:32 +0530

    Create calci.java

---
+++ calci.java
@@ -0,0 +1,26 @@
+pacakage com.company;
+public class calci{
+  public static void main(String[]args)
+  {
+    char operator;
+    switch (operator){
+        case'+' :
+        {
+          return a+b;
+        }
+         case'-' :
+        {
+          return a-b;
+        }
+         case'*' :
+        {
+          return a*b;
+        }
+         case'/' :
+        {
+          return a/b;
+        }
+         case'%' :
+        {
+          return a%b;
+        }
